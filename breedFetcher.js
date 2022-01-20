3beacd68285145745d4fbb8541f48b91fccf7569
pax-n <paxton_nguyen@hotmail.com>
2022-01-20 17:33:06 -0500

    initial commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,25 @@
-//const args = process.argv.slice(2);
+//const args = process.argv.slice(2)
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
-  request (`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`, (error, response, body) => {
+  request (`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,(error, response, body) => {
 
     let data = JSON.parse(body);
-
+    //onsole.log (data)
     if (error) {
+      //console.log(error)
       return callback(error, null);
     }
     if (response.statusCode !== 200) {
       return callback(`Server response: ${response.statusCode}`, null)
-    }
-    
-    if (data[0] === undefined) {
-      console.log("Breed not found");
-    } else {
+    };
+
+    const breed = data[0]
+    if (breed) {
       return callback(null, data[0].description)
-    }  
+    } else {
+      return callback("Breed not found", null);
+    }; 
   })
 };
   
